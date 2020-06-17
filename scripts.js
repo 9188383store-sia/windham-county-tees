ea47dbc84298a5d138deb262764a5b0d9320e0a9
Gabriel Chartier <gabriel@chartier.link>
2020-06-16 22:36:23 -0400

    Resized images, added the "Request a Quote" modal, changed fonts, added netlify forms

---
+++ scripts.js
@@ -0,0 +1,20 @@
+var quoteModal = document.querySelector("#quoteModal")
+var quoteModalOverlay = document.querySelector("#quoteModalOverlay")
+var openQuoteModalBtn = document.querySelector("#requestAQuote")
+var closeQuoteModalBtn = document.querySelector("#closeQuoteModal")
+var cancelQuoteRequestBtn = document.querySelector("#cancelRequest")
+
+openQuoteModalBtn.addEventListener("click", function() {
+    quoteModal.classList.toggle("closed")
+    quoteModalOverlay.classList.toggle("closed")
+})
+
+closeQuoteModalBtn.addEventListener("click", function closeModal() {
+    quoteModal.classList.toggle("closed")
+    quoteModalOverlay.classList.toggle("closed")
+})
+
+cancelQuoteRequestBtn.addEventListener("click", function closeModal() {
+    quoteModal.classList.toggle("closed")
+    quoteModalOverlay.classList.toggle("closed")
+})
