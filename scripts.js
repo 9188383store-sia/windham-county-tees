686a69a4b8c3208709dfbd752795b74dde358bb7
Gabriel Chartier <gabriel@chartier.link>
2020-06-20 16:32:41 -0400

    Added mobile hamburger menu and changed some font sizes

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 var quoteModal = document.querySelector("#quoteModal")
 var quoteModalOverlay = document.querySelector("#quoteModalOverlay")
-var openQuoteModalBtn = document.querySelector("#requestAQuote")
+var openQuoteModalBtn = document.querySelector("#requestQuote")
 var closeQuoteModalBtn = document.querySelector("#closeQuoteModal")
 var cancelQuoteRequestBtn = document.querySelector("#cancelRequest")
+var mobileNav = document.querySelector(".navList")
+var mobileNavIcon = document.querySelector(".mobileNavIcon")
 
 openQuoteModalBtn.addEventListener("click", function() {
     quoteModal.classList.toggle("closed")
@@ -17,4 +19,9 @@ closeQuoteModalBtn.addEventListener("click", function closeModal() {
 cancelQuoteRequestBtn.addEventListener("click", function closeModal() {
     quoteModal.classList.toggle("closed")
     quoteModalOverlay.classList.toggle("closed")
+})
+
+mobileNavIcon.addEventListener("click", function toggleNav() {
+    mobileNav.classList.toggle('open')
+    mobileNavIcon.classList.toggle('mobileMenuOpen')
 })
