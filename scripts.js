b92c171b9453103aefa51f0d0ba21c85478bb5fc
Gabriel Chartier <gabriel@chartier.link>
2020-06-20 23:41:23 -0400

    Styled modal dialog for request a quote

--- scripts.js
+++ scripts.js
@@ -5,20 +5,24 @@ var closeQuoteModalBtn = document.querySelector("#closeQuoteModal")
 var cancelQuoteRequestBtn = document.querySelector("#cancelRequest")
 var mobileNav = document.querySelector(".navList")
 var mobileNavIcon = document.querySelector(".mobileNavIcon")
+var body = document.querySelector("body")
 
 openQuoteModalBtn.addEventListener("click", function() {
     quoteModal.classList.toggle("closed")
     quoteModalOverlay.classList.toggle("closed")
+    body.classList.add("modalOpen")
 })
 
 closeQuoteModalBtn.addEventListener("click", function closeModal() {
     quoteModal.classList.toggle("closed")
     quoteModalOverlay.classList.toggle("closed")
+    body.classList.remove("modalOpen")
 })
 
 cancelQuoteRequestBtn.addEventListener("click", function closeModal() {
     quoteModal.classList.toggle("closed")
     quoteModalOverlay.classList.toggle("closed")
+    body.classList.remove("modalOpen")
 })
 
 mobileNavIcon.addEventListener("click", function toggleNav() {
